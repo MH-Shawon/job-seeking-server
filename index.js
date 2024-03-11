2ba4acf02461b66892801f2d1d9c978ba4727152
Mohsin <mohsin@localhost.localdomain>
2024-03-11 13:46:03 +0600

    single job find

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port =process.env.PORT|| 5000;
 app.use(cors());
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jobseekingass.svd3yy8.mongodb.net/?retryWrites=true&w=majority&appName=JobSeekingAss`;
 
@@ -28,6 +28,12 @@ async function run() {
     app.get('/jobs', async(req,res)=>{
         const result = await jobsCollection.find().toArray();
         res.send(result)
+    })
+    app.get('/jobs/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
+      res.send(result)
     })
      client.db("admin").command({ ping: 1 });
     console.log(
