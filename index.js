1dd0bf5b8974fc0c0715bfa10a12a2fe1e6d4486
Mohsin <mohsin@localhost.localdomain>
2024-03-17 00:27:05 +0600

    toekn fixed

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
+app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
-  })
-);
+  }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -26,6 +24,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
@@ -49,23 +48,25 @@ async function run() {
       .collection("appliedJob");
     const addJobsCollection = client.db("JobSeeking").collection("addJob");
 
-    // jwt related api
 
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      console.log("user for token", user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
+    // jwt related api 
 
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-          sameSite: "none",
-        })
-        .send({ success: true });
-    });
+
+     app.post("/jwt",  async (req, res) => {
+       const user = req.body;
+       console.log("user for token", user);
+       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+         expiresIn: "1h",
+       });
+
+       res
+         .cookie("token", token, {
+           httpOnly: true,
+           secure: false,
+           sameSite: "none",
+         })
+         .send({ success: true });
+     });
 
     app.get("/api/v1/jobs", async (req, res) => {
       const result = await jobsCollection.find().toArray();
@@ -86,9 +87,7 @@ async function run() {
     });
 
     app.get("/api/v1/appliedJobs", verifyToken, async (req, res) => {
-      const queryEmail = req.query.email;
-      const tokenEmail = req.user.email;
-      if (tokenEmail !== queryEmail) {
+      if (req.user.email !== req.query.email) {
         return res.status(403).send({ message: "forbidden access" });
       }
       let query = {};
@@ -107,12 +106,9 @@ async function run() {
     });
 
     app.get("/api/v1/addJobs", verifyToken, async (req, res) => {
-      const queryEmail = req.query.email;
-      const tokenEmail = req.user.email;
-      if (tokenEmail !== queryEmail) {
+      if (req.user.email !== req.query.email) {
         return res.status(403).send({ message: "forbidden access" });
       }
-
       let query = {};
 
       if (req.query?.email) {
